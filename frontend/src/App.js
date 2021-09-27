6ef3a9084b313c45490fdfaeb506e833c1adeb54
Flixirr <filip.0krezel@gmail.com>
2021-09-27 04:46:13 -0700

    Added styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Login from './components/login';
 import Register from './components/register';
 
+import "./styles/main-theme.css";
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,19 +14,22 @@ import {
 
 function App() {
   return (
-    <Router>
-      <Switch>
+    <div style={{ background: "#121212" }} id="main">
+      <Router>
         
-        <Route path="/register">
-          <Register />
-        </Route>
+          <Switch>
 
-        <Route path="/">
-          <Login />
-        </Route>
-        
-      </Switch>
-    </Router>
+            <Route path="/register">
+              <Register />
+            </Route>
+
+            <Route path="/">
+              <Login />
+            </Route>
+            
+          </Switch>
+      </Router>
+    </div>
   );
 }
 
