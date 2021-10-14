6fe16bd3a66b6f3376cdfa7b8ac8bf7866d8aebd
Mateusz_Mandziuk <254303@student.pwr.edu.pl>
2021-10-14 22:41:34 +0200

    hashing added parameters in playlist model

--- backend/groovytunes/models.py
+++ backend/groovytunes/models.py
@@ -18,7 +18,7 @@ class Playlist(models.Model):
     rating_number = models.IntegerField()
     # extra elements needed in Playlist
     # spotify_id = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+    # description = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
